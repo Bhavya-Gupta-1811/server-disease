c515619b25062d4b9a2c840ead842a199b3553d4
Bhavya-Gupta-1811 <bhavya042btcse23@igdtuw.ac.in>
2025-07-07 17:36:27 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const connectDb = require("./utils/db");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH,HEAD",
+  credentials: true,
+}
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 5000;
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
+});
